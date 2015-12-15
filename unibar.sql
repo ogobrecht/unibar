5ca6afa5722f04768379aa2498b1d8d8ce1d83e7
Ottmar Gobrecht <ogobrecht@users.noreply.github.com>
2015-12-15 22:31:47 +0100

    new parameter for filling up the scale

--- unibar.sql
+++ unibar.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE FUNCTION unibar( p_value                  IN NUMBER
                                  , p_scale                  IN NUMBER DEFAULT 1
-                                 , p_width_block_characters IN NUMBER DEFAULT 25 )
+                                 , p_width_block_characters IN NUMBER DEFAULT 25
+                                 , p_fill_scale_01          IN NUMBER DEFAULT 0 )
    RETURN VARCHAR2
    DETERMINISTIC
 IS
@@ -21,29 +22,36 @@ BEGIN
       CASE ROUND(   (   p_value / v_value_of_one_character
                       - FLOOR( p_value / v_value_of_one_character ) )
                   / 0.125 )
-         WHEN 1 THEN -- 1/8
+         WHEN 1 THEN -- 1/8 = char U+258F
             v_return := v_return || UNISTR( '\258F' );
-         WHEN 2 THEN -- 2/8
+         WHEN 2 THEN -- 2/8 = char U+258E
             v_return := v_return || UNISTR( '\258E' );
-         WHEN 3 THEN -- 3/8
+         WHEN 3 THEN -- 3/8 = char U+258D
             v_return := v_return || UNISTR( '\258D' );
-         WHEN 4 THEN -- 4/8
+         WHEN 4 THEN -- 4/8 = char U+258C
             v_return := v_return || UNISTR( '\258C' );
-         WHEN 5 THEN -- 5/8
+         WHEN 5 THEN -- 5/8 = char U+258B
             v_return := v_return || UNISTR( '\258B' );
-         WHEN 6 THEN --6/8
+         WHEN 6 THEN -- 6/8 = char U+258A
             v_return := v_return || UNISTR( '\258A' );
-         WHEN 7 THEN --7/8
+         WHEN 7 THEN -- 7/8 = char U+2589
             v_return := v_return || UNISTR( '\2589' );
-         WHEN 8 THEN --8/8
+         WHEN 8 THEN -- 8/8 = char U+2588
             v_return := v_return || UNISTR( '\2588' );
          ELSE
             NULL;
       END CASE;
    END IF;
 
+   -- fill up scale with shade
+   IF p_fill_scale_01 = 1 THEN
+      FOR i IN 1 .. ( p_width_block_characters - LENGTH( v_return ) ) LOOP
+         v_return := v_return || UNISTR( '\2591' );
+      END LOOP;
+   END IF;
+
    RETURN v_return;
 EXCEPTION
    WHEN VALUE_ERROR THEN
-      RETURN '∞';
+      RETURN UNISTR( '\221E' );
 END;
